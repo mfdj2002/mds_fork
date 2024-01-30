d9cca7c1168b4d91873eda3ed39fbbba4fdc3a1d
jf3516 <jf3516@localhost>
2024-01-29 22:34:59 -0700

    updated setup order

--- env_setup/r7525/new_finish_setup.sh
+++ env_setup/r7525/new_finish_setup.sh
@@ -9,49 +9,35 @@ sudo mount /dev/sda4 /mnt #for r7525
 sudo apt-get install libaio-dev pdsh -y
 
 
-# Create the user SSH directory, just in case.
-# mkdir $HOME/.ssh && chmod 700 $HOME/.ssh
-
-# Retrieve the server-generated RSA private key.
-geni-get key > $HOME/.ssh/id_rsa
-chmod 600 $HOME/.ssh/id_rsa
-
-# Derive the corresponding public key portion.
-ssh-keygen -y -f $HOME/.ssh/id_rsa > $HOME/.ssh/id_rsa.pub
-
-# If you want to permit login authenticated by the auto-generated key,
-# then append the public half to the authorized_keys2 file:
-touch $HOME/.ssh/authorized_keys2
-
-grep -q -f $HOME/.ssh/id_rsa.pub $HOME/.ssh/authorized_keys2 || cat $HOME/.ssh/id_rsa.pub >> $HOME/.ssh/authorized_keys2
-
-
-# sudo apt install python3.8-venv -y
-# cd /mnt
-# sudo python3.8 -m venv ds
-# . /mnt/ds/bin/activate
-# cd
+sudo apt install python3.8-venv -y
+cd /mnt
+sudo python3.8 -m venv ds
+. /mnt/ds/bin/activate
+cd
 
 
 # git clone https://github.com/microsoft/Megatron-DeepSpeed
-# python Megatron-DeepSpeed/setup.py install
+cd Megatron-DeepSpeed
+python setup.py install
+cd ..
 
-# git clone https://github.com/EleutherAI/pile-enron-emails
-# pip install -r pile-enron-emails/requirements.txt
-
-
-# # Install DeepSpeed
-# pip install --upgrade pip
-# pip install --upgrade wheel
-# pip install --upgrade setuptools
-
-# pip install torch torchvision torchaudio
-# pip install --no-cache-dir deepspeed
-# pip install transformers
+git clone https://github.com/EleutherAI/pile-enron-emails
+pip install -r pile-enron-emails/requirements.txt
 
 
-# git clone https://github.com/NVIDIA/apex
-# git checkout 2386a912164b0c5cfcd8be7a2b890fbac5607c82
-# cd apex
-# pip install -v --disable-pip-version-check --no-cache-dir --no-build-isolation --config-settings "--build-option=--cpp_ext" --config-settings "--build-option=--cuda_ext" ./
-# cd
+# Install DeepSpeed
+pip install --upgrade pip
+pip install --upgrade wheel
+pip install --upgrade setuptools
+
+pip install torch torchvision torchaudio
+pip install --no-cache-dir deepspeed
+pip install transformers
+
+
+git clone https://github.com/NVIDIA/apex
+cd apex
+git checkout 2386a912164b0c5cfcd8be7a2b890fbac5607c82
+
+pip install -v --disable-pip-version-check --no-cache-dir --no-build-isolation --config-settings "--build-option=--cpp_ext" --config-settings "--build-option=--cuda_ext" ./
+cd
