f4daa92c082918dacad34349a1644312337726d4
jingkaifu <jf3516@columbia.edu>
2024-02-04 22:16:37 -0500

    added compile apex

--- docker/compile_apex.sh
+++
@@ -1,7 +0,0 @@
-cd /tmp
-git clone https://github.com/NVIDIA/apex
-cd apex
-git checkout 2386a912164b0c5cfcd8be7a2b890fbac5607c82
-
-pip install -v --disable-pip-version-check --no-cache-dir --no-build-isolation --config-settings "--build-option=--cpp_ext" --config-settings "--build-option=--cuda_ext" ./
-cd /home
---
+++ docker/r7525/compile_apex.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ -x "$(command -v nvidia-smi)" ]; then
+    echo "GPUs detected, recompiling Apex with CUDA extensions..."
+    cd /tmp/apex \
+    && git checkout 2386a912164b0c5cfcd8be7a2b890fbac5607c82 \
+    && pip install -v --disable-pip-version-check --no-cache-dir --no-build-isolation --config-settings "--build-option=--cpp_ext" --config-settings "--build-option=--cuda_ext" ./ \
+fi
+
+# Proceed with the original entrypoint command
+exec "$@"
