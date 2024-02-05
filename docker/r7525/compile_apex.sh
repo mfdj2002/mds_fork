33f9118d8fa54981d45d4e5ae76f9382cad3ac30
jingkaifu <jf3516@columbia.edu>
2024-02-04 22:24:26 -0500

    added entrypoint

--- docker/r7525/compile_apex.sh
+++ docker/r7525/compile_apex.sh
@@ -5,6 +5,7 @@ if [ -x "$(command -v nvidia-smi)" ]; then
     cd /tmp/apex \
     && git checkout 2386a912164b0c5cfcd8be7a2b890fbac5607c82 \
     && pip install -v --disable-pip-version-check --no-cache-dir --no-build-isolation --config-settings "--build-option=--cpp_ext" --config-settings "--build-option=--cuda_ext" ./ \
+    && cd /
 fi
 
 # Proceed with the original entrypoint command
